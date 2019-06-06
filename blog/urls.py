7d62c59b6571e234128202618d4ae13a6f7656e5
xingnn <xingnn@tuya.com>
2019-06-06 18:05:09 +0800

    路由

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# @Time    : 2019/6/6 17:57
+# @Author  : Nana Xing
+# @File    : urls.py
+# @ProjectName: myDjangoProject
+# @Software : PyCharm
+# @Description :url和对应的视图函数
+
+from django.conf.urls import url
+# 从当前目录下导入 views 模块
+from  . import views
+
+urlpatterns=[
+    url(r'^$',views.index,name='index')
+]
