c99e53348116db6cb96ad7baa55a418d63149184
xingnn <xingnn@tuya.com>
2019-06-28 11:16:20 +0800

    update

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Post
 
@@ -13,5 +12,6 @@ def index(request):
     """
     # return HttpResponse('欢迎访问我的博客首页')
     # return render(request,'blog/index.html',context={'title':'NaNa的博客首页','welcome':'欢迎访问我的博客首页'})
+    # 逆序,- 号表示逆序
     post_list=Post.objects.all().order_by('-create_time')
     return render(request,'blog/index.html',context={'post_list':post_list})
