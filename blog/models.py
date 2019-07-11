2dc9f5a0a1daecb3519151b6359ab0d0ec612ee1
xingnn <xingnn@tuya.com>
2019-07-11 19:48:37 +0800

    add | 自动生成摘要文章

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from django.urls import reverse
+from django.utils.html import strip_tags
+import markdown
 
 
 class Category(models.Model):
@@ -27,13 +28,14 @@ class Category(models.Model):
     Django 内置的全部类型可查看文档：
     https://docs.djangoproject.com/en/1.10/ref/models/fields/#field-types
     """
-    verbose_name='分类'
-    verbose_name_plural='分类管理'
+    verbose_name = '分类'
+    verbose_name_plural = '分类管理'
+
     def __str__(self):
         return 'name:%s' % self.name
 
     # 分类名
-    name = models.CharField(max_length=100,verbose_name='分类')
+    name = models.CharField(max_length=100, verbose_name='分类')
 
 
 class Tag(models.Model):
@@ -42,6 +44,7 @@ class Tag(models.Model):
     """
     verbose_name = '标签'
     verbose_name_plural = '标签管理'
+
     def __str__(self):
         """
         对象描述
@@ -50,7 +53,7 @@ class Tag(models.Model):
         return 'name:%s' % self.name
 
     # 标签名
-    name = models.CharField(max_length=100,verbose_name='标签名')
+    name = models.CharField(max_length=100, verbose_name='标签名')
 
 
 class Post(models.Model):
@@ -59,15 +62,16 @@ class Post(models.Model):
     """
     verbose_name = '文章'
     verbose_name_plural = '文章管理'
+
     def __str__(self):
         return self.title
 
     class Meta:
         # ordering 属性用来指定文章排序方式
-        ordering=['-create_time','title']
+        ordering = ['-create_time', 'title']
 
     # 文章标题
-    title = models.CharField(max_length=70,verbose_name='文章标题')
+    title = models.CharField(max_length=70, verbose_name='文章标题')
 
     # 文章正文 使用TextField 存储大段文本
     body = models.TextField(verbose_name='正文')
@@ -78,22 +82,33 @@ class Post(models.Model):
     modified_time = models.DateTimeField('修改时间')
 
     # 文章摘要，可以为空
-    excerpt = models.CharField(max_length=200, blank=True,verbose_name='文章摘要')
+    excerpt = models.CharField(max_length=200, blank=True, verbose_name='文章摘要')
 
     # 文章分类，把文章对应的数据库表和分类对应的数据库表关联起来
     # 一个分类下有多个文章，一个文章对应一个分类。一对多的关系，用外键
-    category = models.ForeignKey(Category, on_delete=models.CASCADE,verbose_name='分类')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='分类')
 
     # 文章标签，把文章对应的数据库表和标签对应的数据库表关联起来
     # 一篇文章对应多个标签，一个标签对应多个文章。多对多的关系
     # 文章可以没有标签，可以为空
-    tags = models.ManyToManyField(Tag, blank=True,verbose_name='文章标签')
+    tags = models.ManyToManyField(Tag, blank=True, verbose_name='文章标签')
 
     # 文章作者 User 是从 django.contrib.auth.models 导入的。
     # django.contrib.auth.models 是django内置的应用，专门用于处理网站用户的注册、登录等流程
-    author = models.ForeignKey(User, on_delete=models.CASCADE,verbose_name='作者')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='作者')
 
-    views=models.PositiveIntegerField(default=0,verbose_name='浏览量')
+    views = models.PositiveIntegerField(default=0, verbose_name='浏览量')
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.markdown(self.body[:54],extensions=[
+                'extra',
+                'codehilite'
+            ])
+            # 先将body渲染成html，然后strip_tags去掉html标签，然后取前54个字符
+            self.excerpt = strip_tags(md)
+            # 调用父类save方法，保存到数据库
+            super(Post, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         """
@@ -102,10 +117,8 @@ class Post(models.Model):
         """
         # blog应用下name=detail的视图函数
         # url反向解析 找到urls.py中name='detail'的路径
-        return reverse('blog:detail',kwargs={'pk':self.pk})
+        return reverse('blog:detail', kwargs={'pk': self.pk})
 
     def increase_views(self):
-        self.views+=1
+        self.views += 1
         self.save(update_fields=['views'])
-
-
